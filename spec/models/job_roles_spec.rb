25cb8c7c29ea03060ecb7c1a454fac9d86020cdf
Stefan Ritchie <stefanritchie@gmail.com>
2022-06-27 21:58:59 +0100

    Update specs

--- spec/models/job_roles_spec.rb
+++ spec/models/job_roles_spec.rb
@@ -14,9 +14,6 @@ RSpec.describe JobRole, type: :model do
     it { should validate_presence_of(:company) }
     it { should validate_presence_of(:job_title) }
     it { should validate_presence_of(:subtitle) }
-    it { should validate_presence_of(:start_date) }
-    it { should validate_presence_of(:end_date) }
-    it { should validate_presence_of(:summary) }
   end
 
   context 'associations' do
